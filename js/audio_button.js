04244288fed50290dfb77a4e387b119005f8a96d
Kareem Adel <kareem.adel.000@gmail.com>
2018-01-05 17:15:31 +0200

    handling minor style warnings

--- js/audio_button.js
+++ js/audio_button.js
@@ -1,18 +1,16 @@
 var audio = document.getElementById("player");
 var button = document.getElementById("playPause");
 button.style.outline = "none";
-audio.play()
+audio.play();
 var isPlay = true;
 var togglePlay = function(e) {
     e.preventDefault();
     button.classList.toggle('mute');
     if (isPlay) {
         audio.pause();
-        button.src = "assets/images/audio_off.png";
     } else {
         audio.play();
-        button.src = "assets/images/audio_on.png";
     }
     isPlay = !isPlay;
-}
+};
 button.addEventListener('click', togglePlay);
