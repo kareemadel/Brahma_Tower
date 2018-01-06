9ed3fa57538eb3cd3c14e9fa7a9e7ac9f587a895
Kareem Adel <kareem.adel.000@gmail.com>
2018-01-06 09:10:56 +0200

    corrected audio script to work with new structure, applied a beautifier of the pages.

--- js/audio_button.js
+++ js/audio_button.js
@@ -1,11 +1,13 @@
 var audio = document.getElementById("player");
-var button = document.getElementById("playPause");
+var button = document.getElementsByClassName("speaker");
 audio.loop = true;
 audio.play();
 var isPlay = true;
 var togglePlay = function(e) {
     e.preventDefault();
-    button.classList.toggle('mute');
+    for (var i = button.length - 1; i > -1; i--) {
+        button[i].classList.toggle('mute');
+    }
     if (isPlay) {
         audio.pause();
     } else {
@@ -13,13 +15,13 @@ var togglePlay = function(e) {
     }
     isPlay = !isPlay;
 };
-button.addEventListener('click', togglePlay);
+
+for (var i = button.length - 1; i > -1; i--) {
+    button[i].addEventListener('click', togglePlay);
+}
 
 var flip = function(current_page, target_page) {
     for (var i = 1; i < 7; i++) {
-        /*if (i == 3) {
-            continue;
-        }*/
         document.getElementById("page" + i).style.display = "none";
     }
     document.getElementById("page" + target_page).style.display = "";
