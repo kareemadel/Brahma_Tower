f16aaf40a8a5a8c00bf55bc4f2afb8f3189c50e8
shaimaa_nabil <shaimaahussin44@yahoo.com>
2018-01-10 08:53:00 +0200

    adding function for buttons in page1 and page2 ..changing in html and css

--- js/buttons_And_Their_Functions.js
+++ js/buttons_And_Their_Functions.js
@@ -5,7 +5,6 @@ function flip(current_page, target_page) {
     document.getElementById("page" + target_page).style.display = "";
 }
 
-flip(0,1);
 
 //////////////////////////////////////////////////////////////
 //////// All Clickables On All Pages As A Global Object///////
@@ -28,14 +27,17 @@ var clickables = {
                 easy : document.getElementById("easy"),
                 average : document.getElementById("average"),
                 hard : document.getElementById("hard"),
-                insane : document.getElementById("insane")
+                insane : document.getElementById("insane"),
+
             },
 
     page5 : {
                 charmander : document.getElementById("charmander"),
                 pikachu : document.getElementById("pikachu"),
                 bulbasaur : document.getElementById("bulbasaur"),
-                squirtle : document.getElementById("squirtle")
+                squirtle : document.getElementById("squirtle"),
+                mainMenuButton : document.getElementById("mainMenuButton01")
+
 
             },
 
@@ -66,10 +68,12 @@ function onclickAttrIntializer() {
     for (k in clickables.page5) {
         clickables.page5[k].addEventListener('click', character);
     }
+    console.log("asdf");
+    clickables.page5.mainMenuButton.addEventListener('click', mainMenuButtonjj);
     clickables.page6.restartBtn.addEventListener('click', restart_btn);
-    // clickables.page6.tower1.addEventListener('click', handleTowers);
-    // clickables.page6.tower2.addEventListener('click', handleTowers);
-    // clickables.page6.tower3.addEventListener('click', handleTowers);
+    clickables.page6.tower1.addEventListener('click', handleTowers);
+    clickables.page6.tower2.addEventListener('click', handleTowers);
+    clickables.page6.tower3.addEventListener('click', handleTowers);
 }
 //page 1
 function Submit_btn(e) {
@@ -81,6 +85,20 @@ function Submit_btn(e) {
     */
 
     // body...
+    function validateName (IdForTextInput)
+    {
+        var content = document.getElementById(IdForTextInput).value;
+        var pattern = new RegExp("^[a-zA-Z]{3,15}$");
+        return pattern.test(content);
+    }
+    e.preventDefault();
+    if (!validateName("playerName"))
+    {
+        alert("too short");
+    }
+    else {
+      flip(0,2)
+    }
 
 }
 
@@ -93,7 +111,6 @@ function Start_btn(e) {
     */
 
     // body...
-    flip(2, 3);
 }
 
 // page 3
@@ -103,14 +120,14 @@ function mainMenu_btn() {
     takes only one parameter, which is the calling event.
     switch to page2
     */
-    flip(3,2);
+    flip(000,2)
 }
 
 function skip_btn() {
     /*
     switch to page4
     */
-    flip(3,4);
+    flip(000,4)
 }
 
 ////////////////////////////////
@@ -137,6 +154,10 @@ function character(e) {
     */
 
     // body...
+}
+
+function mainMenuButtonjj(){
+  flip(0,6)
 }
 
 //////////////////////////////////
